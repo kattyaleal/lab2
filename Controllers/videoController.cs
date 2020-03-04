987186e415ccea3ec22dbe6f959e8026f8b0806c
kattya leal <kattyaleal26@hotmail.com>
2020-03-03 19:59:36 -0600

    agregar

---
+++ Controllers/videoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+
+namespace MVCLaboratorio.Controllers
+{
+    public class videoController : Controller
+    {
+        //
+        // GET: /video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(int idVideo, String titulo, int reproducciones, string url)
+        {
+            //guardar
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@reproducciones", reproducciones));
+            parametros.Add(new SqlParameter("@url", url));
+
+            BaseHelper.ejecutarSentencia("sp_videos_insetar", CommandType.StoredProcedure, parametros);
+
+            return RedirectToAction("Index", "video");
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int idVideo)
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int idVideo, String titulo, int reproducciones, string url)
+        {
+            return View();
+        }
+
+    }
+}
